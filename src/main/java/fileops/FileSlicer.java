dce3f1b7cf5bdc64de69f08493a2c98e31e8620e
Tikitikitikidesuka <deesneakygerbil@gmail.com>
2024-02-02 18:54:37 +0100

    Add FileSlice class. It does not work yet.

---
+++ src/main/java/fileops/FileSlicer.java
@@ -0,0 +1,42 @@
+package fileops;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileSlicer {
+    public List<byte[]> sliceFile(String inputFilePath) {
+        List<byte[]> fileSlices = new ArrayList<>();
+        File inputFile = new File(inputFilePath);
+        int partSize = 1024; // 1 kB
+
+        try (FileInputStream inputStream = new FileInputStream(inputFile)) {
+            byte[] buffer = new byte[partSize];
+            int bytesRead = 0;
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            while ((bytesRead = inputStream.read(buffer)) > 0) {
+                if (outputStream.size() + bytesRead > partSize) {
+                    fileSlices.add(outputStream.toByteArray());
+                    outputStream.reset();
+                }
+                outputStream.write(buffer, 0, bytesRead);
+
+                if (outputStream.size() >= partSize) {
+                    fileSlices.add(outputStream.toByteArray());
+                    outputStream.reset();
+                }
+            }
+
+            if (outputStream.size() > 0) {
+                fileSlices.add(outputStream.toByteArray());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileSlices;
+    }
+}
