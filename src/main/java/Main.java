dc66102c7af8955480885b4bdcd97355175efa26
GuilledlC <guillermodelacalhacar@gmail.com>
2024-02-02 19:50:38 +0100

    Add jackson .json dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fileops.FileSlicer;
 import fileops.FileStitcher;
 import sender.Downloader;
@@ -8,7 +9,6 @@ import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
-
 		Sender sender = new Sender("https://discord.com/api/webhooks/1203027726647427133/dxTtB9Z9K5vDn-zKhNmlzg66YAUZ44oDbVhaazC7DZpP6iI1iRfqawf14_R0xVmlokGM");
 		ArrayList<String> urls = sender.send(FileSlicer.sliceFile("./src/main/resources/aldeaoindemoniado.png"));
 		FileStitcher.stitchFile(Downloader.downloadFile(urls), "filename.png");
