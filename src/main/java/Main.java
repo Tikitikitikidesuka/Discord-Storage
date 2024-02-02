0cc79f343f561c4deb9a6a99e15f928da04fe81d
alephandro <alex29561@gmail.com>
2024-02-02 18:54:53 +0100

    Implement send and read with webhook

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import club.minnced.discord.webhook.WebhookClient;
+import club.minnced.discord.webhook.receive.ReadonlyAttachment;
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // Using the builder
+        WebhookClient client = WebhookClient.withUrl("https://discord.com/api/webhooks/1203027726647427133/dxTtB9Z9K5vDn-zKhNmlzg66YAUZ44oDbVhaazC7DZpP6iI1iRfqawf14_R0xVmlokGM"); // or withId(id, token)
+        File file = new File(".gitignore");
+
+        long long1 = client.send(file, ".gitignore").get().getId();
+
+        ReadonlyAttachment i = client.get(long1).get().getAttachments().get(0);
+
+        System.out.println(i.getUrl());
+
+        client.close();
     }
 }
