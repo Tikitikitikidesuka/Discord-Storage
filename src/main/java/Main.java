2cb9df0d649762e458c4b5d5efb66cb32ef2c826
GuilledlC <guillermodelacalhacar@gmail.com>
2024-02-02 19:09:46 +0100

    Add Beetle Tank

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
-import club.minnced.discord.webhook.WebhookClient;
-import club.minnced.discord.webhook.receive.ReadonlyAttachment;
+import fileops.FileSlicer;
 import sender.Sender;
 
-import java.io.File;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
 		Sender sender = new Sender("https://discord.com/api/webhooks/1203027726647427133/dxTtB9Z9K5vDn-zKhNmlzg66YAUZ44oDbVhaazC7DZpP6iI1iRfqawf14_R0xVmlokGM");
-		sender.send(".gitignore");
+		FileSlicer fs = new FileSlicer();
+
+		sender.send(fs.sliceFile("./src/main/resources/beetle-tank.png"));
     }
 }
--- src/main/java/sender/Sender.java
+++ src/main/java/sender/Sender.java
@@ -3,6 +3,8 @@ package sender;
 import club.minnced.discord.webhook.WebhookClient;
 import club.minnced.discord.webhook.receive.ReadonlyAttachment;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class Sender {
@@ -20,6 +22,22 @@ public class Sender {
 		long msgID = client.send(file, ".gitignore").get().getId();
 		ReadonlyAttachment attachment = client.get(msgID).get().getAttachments().get(0);
 		System.out.println(attachment.getUrl());
+
+		client.close();
+	}
+
+	public void send(List<byte[]> bytes) throws ExecutionException, InterruptedException {
+
+		int c = 0;
+		ArrayList<Long> msgIDs = new ArrayList<>();
+		for(byte[] b : bytes) {
+			msgIDs.add(client.send(b, String.valueOf(c++)).get().getId());
+		}
+
+		for(Long l : msgIDs) {
+			ReadonlyAttachment attachment = client.get(l).get().getAttachments().get(0);
+			System.out.println(attachment.getUrl());
+		}
 
 		client.close();
 	}
